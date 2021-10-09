03270a3bdac280644cff5006efbb5db38ef7d5a7
Three97 <jason.fedler@gmail.com>
2021-10-09 16:24:08 -0400

    updates footer component to include footer value from toc

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react';
+import toc from '../toc.json';
 
 const Footer: FC = function() {
   const currentYear:number = new Date().getFullYear();
@@ -8,6 +9,9 @@ const Footer: FC = function() {
 
   return (
     <footer className="mt-5 p-3 text-center fg-highlight3" role="contentinfo">
+      <div>
+        {toc.footer}
+      </div>
       <div className="mt-3">
         <em>(c) {yearRange} Copyright JasonFedler.com</em>
       </div>
