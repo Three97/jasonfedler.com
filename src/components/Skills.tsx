806212bfd06a8736a1c34d4cefd82e5c556b559a
Three97 <jason.fedler@gmail.com>
2021-10-16 20:34:07 -0400

    adds a little more padding to Skills header;

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -7,7 +7,7 @@ const Skills: FC<any> = function({ items }) {
   return (
     <div className="container mb-5">
       <div className="row mb-1">
-        <div className="col-lg-3 col-md-4 text-end fw-bold pe-3">Skill</div>
+        <div className="col-lg-3 col-md-4 text-end fw-bold pe-4">Skill</div>
         <div className="col-lg-1 d-none d-lg-block text-center fw-bold">Discipline</div>
         <div className="col-md-8 text-start fw-bold">Proficiency</div>
       </div>
