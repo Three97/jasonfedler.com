478cbcdcd3d45e4c9f34a3e78c70e391c912ae02
Jason Fedler <Jason.Fedler@gmail.com>
2023-07-24 20:47:23 -0400

    adds a synopsis setion under banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import toc from "./toc.json";
 import Banner from "./components/Banner";
+import Synopsis from "./components/Synopsis";
 import Skills from "./components/Skills";
 import Cards from "./components/Cards"
 import Footer from "./components/Footer";
@@ -10,6 +11,7 @@ function App() {
     <>
       <Banner title={toc.title} subtitle={toc.subtitle} tagline={toc.tagline} />
       <main role="main">
+        <Synopsis synopsis={toc.synopsis} />
         <Skills items={toc.skills} />
         <Cards props={toc.cards} />
       </main>
---
+++ src/components/Synopsis.tsx
@@ -0,0 +1,19 @@
+import { FC } from 'react';
+
+const Synopsis: FC<any> = function({ synopsis }) {
+  return (
+    <div className="synopsis-statement py-4 mb-3">
+      <div className="container">
+        <div className="row">
+          <div className="col-2"></div>
+          <div className="col-8">
+            {synopsis}
+          </div>
+          <div className="col-2"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Synopsis;
