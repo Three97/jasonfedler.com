64541335839dd42641b40023bb1f70a0f6c01fa5
Jason Fedler <Jason.Fedler@gmail.com>
2023-08-13 16:26:42 -0400

    make github and linkedin icons a touch larger;

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -3,6 +3,7 @@ import { FC } from 'react';
 const Banner: FC<any> = function({ title, subtitle, tagline }) {
   // want to handle links of the sort in this component differently than the rest of the page
   const anchorStyle = Object.assign({}, { padding: "0"}, { backgroundColor: "transparent" });
+  const imgSize = "48px";
   return (
     <header role="banner">
       <div className="jumbotron">
@@ -12,12 +13,12 @@ const Banner: FC<any> = function({ title, subtitle, tagline }) {
           <div className="row">
             <div className="col-6 text-end" title="All my sh*t is online">
               <a target="_blank" href="https://github.com/Three97" rel="noreferrer" style={anchorStyle}>
-                <img src="./assets/github.png" alt="GitHub logo" width="36px" />
+                <img src="./assets/github.png" alt="GitHub logo" width={imgSize} />
               </a>
             </div>
             <div className="col-6 text-start" title="21st century resume that I don't keep up with near as much as I should">
               <a target="_blank" href="https://www.linkedin.com/in/jason-fedler-35b33a8/" rel="noreferrer" style={anchorStyle}>
-                <img src="./assets/linkedin.png" alt="LinkedIn logo" width="36px" />
+                <img src="./assets/linkedin.png" alt="LinkedIn logo" width={imgSize} />
               </a>
             </div>
           </div>
