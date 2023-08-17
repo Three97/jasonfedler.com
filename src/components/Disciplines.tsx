da773dd5b0d3646bfba8d215e682202b478a034c
Jason Fedler <Jason.Fedler@gmail.com>
2023-08-16 21:20:47 -0400

    enforces types for component input values, replaces anchor tags with Link component, tweaks some link padding; tweaks some link coloring; introduces SocialLink component

--- src/components/Disciplines.tsx
+++ src/components/Disciplines.tsx
@@ -5,6 +5,7 @@ import SimpleSkill from "./SimpleSkill";
 import GetDisciplineByDisciplineType from "../functions/GetDisciplineByDisciplineType";
 import Courses from "./Courses";
 import ICourse from "../interfaces/ICourse";
+import Link from "./Link";
 
 const Disciplines: FC<{ skills: ISkill[]; courses: ICourse[];}> = function ({ skills, courses }) {
   const distinctDisciplines = Array.from(new Set(skills.map((a) => a.discipline)));
@@ -17,9 +18,8 @@ const Disciplines: FC<{ skills: ISkill[]; courses: ICourse[];}> = function ({ sk
         and is one of the main reasons I gravitated to this career. It seems
         like the more we know, the more we know we don't know. The proficiency 
         values below align to the knowledge scale defined&nbsp;
-        <a href="https://hr.uiowa.edu/careers/competencies/proficiency-levels" target="_blank" rel="noreferrer">
-          here
-        </a>. I have a personal Pluralsight license that I use to keep up with any
+        <Link name="here" url="https://hr.uiowa.edu/careers/competencies/proficiency-levels" tooltip="" />.
+        I have a personal Pluralsight license that I use to keep up with any
         new technology I am exposed to. I'm my worst critic and tend to 
         underestimate my abilities while overdelivering results.
       </p>
--- src/components/SimpleSkill.tsx
+++ src/components/SimpleSkill.tsx
@@ -1,17 +1,16 @@
 import { FC } from "react";
 import GetProficiencyLevel from '../functions/GetProficiencyLevel';
+import Link from "./Link";
+import ISkill from "../interfaces/ISkill";
 
-const SimpleSkill: FC<any> = function({ site, skill, discipline, proficiency }) {
-  const styleForSkill = {
-    padding: "0.25em 1em"
-  }
+const SimpleSkill: FC<ISkill> = function({ site, skill, discipline, proficiency }) {
   const profLevel = GetProficiencyLevel(proficiency);
   //const classNameForProficiency = "text-start " + profLevel.toLowerCase() + "-" + discipline;
   const classNameForProficiency = `text-start ${profLevel.toLowerCase()}-${discipline}`;
   return (
     <div className="row mb-1">      
       <div className="col-offset-1 col-md-4 col text-end code">
-        <a href={site} style={styleForSkill} title={(site !== "") ? "Click to find out more" : ""}>{skill}</a>
+        <Link name={skill} url={site} tooltip={(site !== "") ? "Click to find out more" : ""} />
       </div>
       <div className="col-md-7 col">
         <div className="bg-light w-100">
