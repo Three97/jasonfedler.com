ff16e39614bafa0034a665825511d1704cc6b32f
Jason Fedler <Jason.Fedler@gmail.com>
2024-08-11 13:18:48 -0400

    grows the skill meter to file the frame a little better

--- src/components/SimpleSkill.tsx
+++ src/components/SimpleSkill.tsx
@@ -13,7 +13,7 @@ const SimpleSkill: FC<ISkill> = function({ helpUrl, name, discipline, proficienc
       <div className="col-offset-1 col-md-4 col text-end code">
         <Link name={name} url={helpUrl} tooltip={(helpUrl !== "") ? "Click to find out more" : ""} />
       </div>
-      <div className="col-md-7 col">
+      <div className="col-md-8 col">
         <div className="skill-outline">
           <div className="bg-light" style={{ width: `calc(${proficiency}%` }}>
             <div className={classNameForProficiency} title={profLevel + " knowledge of " + name + " with a rating of " + proficiency}>
